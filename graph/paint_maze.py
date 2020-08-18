30661e9786d2af1a5161fa5125a407f0230ead6a
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-18 00:52:50 -0300

    paint

---
+++ graph/paint_maze.py
@@ -0,0 +1,19 @@
+def paint(maze, x, y):
+    def paint_path(maze, x, y, color):
+        if not (0<=x<len(maze) and 0<=y<len(maze[0]) and  maze[x][y] == color):
+            return 
+        maze[x][y] = 1 - color
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        for dir in directions:
+           paint_path(maze, x + dir[0], y + dir[1], color)
+    color = maze[x][y]
+    paint_path(maze, x, y, color)
+
+
+maze = [ [1,1,0,0],
+         [0,1,1,0],
+         [0,0,1,0],
+         [0,0,1,0]]
+
+
+paint(maze, len(maze)-1, 0)
