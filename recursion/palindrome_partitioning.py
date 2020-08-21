ef00628cba23726f04038295a2b4a7bcc2cb74d3
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-20 21:50:07 -0300

    palindrome

---
+++ recursion/palindrome_partitioning.py
@@ -0,0 +1,32 @@
+'''
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+'''
+def palindrome_partitioning(word):
+    temp = []
+    ans = []
+    def is_palindrome(word):
+        if word:
+            return word == word[::-1]
+    def recurse(word, temp, ans):
+        if not word:
+           ans.append(temp[:])
+        for i in range(1, len(word)+1):
+            if is_palindrome(word[:i]): 
+                temp.append(word[:i])
+                
+                recurse(word[i:], temp, ans)
+
+                temp.pop()
+    recurse(word, temp, ans)
+    print(ans)
+            
+
+    
+
+word = "aab"
+palindrome_partitioning(word)
