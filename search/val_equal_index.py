d93ad7eb00126727ed2181d85f43cb7c8e6188d4
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-21 03:33:38 -0300

    val_equal

---
+++ search/val_equal_index.py
@@ -0,0 +1,23 @@
+'''
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+'''
+def val_equal_index(arr):
+    l = 0
+    h = len(arr)-1
+    while l <= h:
+        mid = (h+l)//2
+        if mid == arr[mid]:
+            return mid
+        elif arr[mid] < mid:
+            l = mid
+        else:
+            h = mid
+    return
+
+arr = [-2, 0, 2, 5, 6, 7, 9]
+val_equal_index(arr)
