12c5e060bddfd21713d3713e9fc35cf59a17981a
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-21 04:13:28 -0300

    shifted

---
+++ search/shifted_array.py
@@ -0,0 +1,21 @@
+'''
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+'''
+def val_equal_index(arr):
+    l = 0
+    h = len(arr)-1
+    while l < h:
+        mid = (h+l)//2
+        if arr[mid] > arr[h]:
+            l = mid + 1
+        else:
+            h = mid
+    return l
+
+arr = [378, 478, 550, 631, 103, 203, 220, 234, 279, 368]
+val_equal_index(arr)
