d49b8c9c308b3100ebf6882a864ee0b545a6123f
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-23 23:47:51 -0300

    kth smallest

---
+++ binarysearchtree/kth_smallest_bst.py
@@ -0,0 +1,51 @@
+'''
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+
+'''
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val 
+        self.left = None
+        self.right = None
+#number of elements greater than 1
+def kth_smallest_bst(root, k):
+    stack = []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root
+        root = root.right
+        
+    
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(6)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.left.left.left = TreeNode(1)
+kth_smallest_bst(root, 3)
