9e79c82547e9e20f3da786fa36db0be846f86196
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-08-31 04:42:45 -0300

    palindrome

---
+++ dynamicprogramming/palindrome_subsequence_recursion.py
@@ -0,0 +1,10 @@
+def palindrome_subsequence(s):
+    def dp(i,j):
+        if i>j:
+            return 0
+        if i==j:
+            return 1
+        if s[i]==s[j]:
+            return dp(i+1,j-1)+2
+        return max(dp(i+1,j),dp(i,j-1))
+    return dp(0,len(s)-1)
