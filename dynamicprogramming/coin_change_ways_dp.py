0f0fad9a86032759dc354c3e49294c6d3e2f34a1
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-09-06 22:06:56 -0300

    coint change ways dp

--- dynamicprogramming/coin_change_ways_dp.py
+++ dynamicprogramming/coin_change_ways_dp.py
@@ -1,8 +1,12 @@
 def change(amount, coins):
-    dp = [0] * (amount + 1)
-    dp[0] = 1
-    
-    for coin in coins:
-        for x in range(coin, amount + 1):
-            dp[x] += dp[x - coin]
-    return dp[amount]
+    n = len(coins)
+    M = [[0 for i in range(amount+1)] for x in range(n+1)]
+    for i in range(n+1):
+        M[i][0] = 1
+    for i in range(1, n+1):
+        for j in range(1, amount+1):
+           if coins[i-1] > j:
+                M[i][j] = M[i-1][j]
+           else:
+                M[i][j] = M[i-1][j] + M[i][j-coins[i-1]]
+           return M[n][amount]
