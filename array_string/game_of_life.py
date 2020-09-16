503ca7792b4b53fb95972524d7fc40c9e901f7fc
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-09-16 01:40:16 -0300

    array

---
+++ array_string/game_of_life.py
@@ -0,0 +1,32 @@
+DEAD = 0
+ALIVE = 1
+ALIVE_TO_DEAD = 2
+DEAD_TO_ALIVE = 3
+class Solution:
+    def gameOfLife(board):
+        """
+        :type board: List[List[int]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        def count_neighbours(grid, i , j):
+            count = 0
+            directions = [(-1,1), (-1,0), (-1,-1), (1,1), (1, 0), (1,-1), (0,1), (0,-1)]
+            for dir in directions:
+                next_x, next_y = i + dir[0], j + dir[1]
+                if next_x >= 0 and next_x < len(grid) and next_y >= 0 and next_y < len(grid[0]) and grid[next_x][next_y] in (ALIVE, ALIVE_TO_DEAD):
+                    count += 1
+            return count
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                curr_count = count_neighbours(board, row, col)
+                if board[row][col] == ALIVE and not (curr_count == 2 or curr_count == 3):
+                    board[row][col] = ALIVE_TO_DEAD
+                if board[row][col] == DEAD and curr_count == 3:
+                    board[row][col] = DEAD_TO_ALIVE
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == ALIVE_TO_DEAD:
+                    board[row][col] = 0
+                if board[row][col] == DEAD_TO_ALIVE:
+                    board[row][col] = 1
