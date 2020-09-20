a084d3696f2905e92269f245a183e55b736f2e4a
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-09-20 02:15:02 -0300

    trees

---
+++ tree/merge_treenode.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 and not t2:
+            return None
+        if not t1 or not t2:
+            return t1 or t2
+        root = TreeNode(t1.val + t2.val)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        root.right = self.mergeTrees(t1.right, t2.right)
+        return root
