4e6adec08bfc7deeef4789125fa46675111f9aa1
Rodrigo Proença <rodrigoproenca@Rodrigos-MacBook-Pro.local>
2020-09-21 19:07:33 -0300

    first ocurrence

--- search/firstoccurence.py
+++ search/firstoccurence.py
@@ -1,25 +1,16 @@
-'''
-Input: "aab"
-Output:
-[
-  ["aa","b"],
-  ["a","a","b"]
-]
-'''
 def first_ocurrunce(arr, k):
     l = 0
     h = len(arr)-1
     while l <= h:
         mid = (h+l)//2
-        if mid == 0 or (arr[mid] == k and arr[mid-1] != arr[mid]):
+        if arr[mid] == k and (mid == 0 and arr[mid-1] != arr[mid]):
             return mid
         elif arr[mid] < k:
-            l = mid
+            l = mid + 1
         else:
             h = mid
-    return
+    return -1
 
-arr = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
-arr1 = [285]
-k = 1
-first_ocurrunce(arr1, k)
+arr = [0, 1]
+ans = first_ocurrunce(arr, 1)
+print(arr)
