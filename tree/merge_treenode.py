424a3ff0d621902b9bed97c6cc07c1a6edd56211
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-02-19 22:03:47 -0300

    Update merge_treenode.py

--- tree/merge_treenode.py
+++ tree/merge_treenode.py
@@ -4,7 +4,8 @@
 PROBLEM STATEMENT:
 https://leetcode.com/problems/merge-two-binary-trees/
 ''' 
-def __init__(self, val=0, left=None, right=None):
+class Treenode:
+     def __init__(self, val=0, left=None, right=None):
          self.val = val
          self.left = left
          self.right = right
