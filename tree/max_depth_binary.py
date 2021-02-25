a606e9248b5d4b3f2cc05105a1b207e740bc9d4a
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-02-24 23:02:55 -0300

    Update max_depth_binary.py

--- tree/max_depth_binary.py
+++ tree/max_depth_binary.py
@@ -4,11 +4,11 @@ class TreeNode:
          self.val = val
          self.left = left
          self.right = right
-class Solution:
-    def maxDepth(self, root):
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        else:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+def maxDepth(root):
+   if not root:
+       return 0
+   if not root.left and not root.right:
+       return 1
+   else:
+       return 1 + max(maxDepth(root.left), maxDepth(root.right))
