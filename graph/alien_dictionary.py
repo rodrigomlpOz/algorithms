d8e70547d156e027ff3363f0e0e5b0d9d510ea9d
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-07-07 20:51:05 -0300

    Update alien_dictionary.py

--- graph/alien_dictionary.py
+++ graph/alien_dictionary.py
@@ -9,22 +9,15 @@ class Solution(object):
         self.add_vertices(w1, graph)
         self.add_vertices(w2, graph)        
         min_length = min(len(w1), len(w2))
-        found = False
         for i in range(min_length):
             if w1[i] != w2[i]:
                 graph[w1[i]].add(w2[i])
-                found = True
                 break
-        if found == False and len(w1) > len(w2):
-            return False # "abstract", "abs" is an error. But "abs", "abstract" is perfectly fine.
-        return True
 
     def build_graph(self, words):
         graph = {}
         for i in range(len(words)-1):
             w1, w2 = words[i], words[i+1]
-            if not self.add_words_to_graph(graph, w1, w2):
-                return {}
         self.add_vertices(words[-1], graph)
         return graph
 
