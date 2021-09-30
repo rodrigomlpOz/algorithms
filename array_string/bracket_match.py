8107dc8b6c3a7f47a8eaf0c9d325fa0f533ed2dc
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-09-30 19:17:01 -0400

    Update bracket_match.py

--- array_string/bracket_match.py
+++ array_string/bracket_match.py
@@ -1,3 +1,10 @@
+'''
+A string of brackets is considered correctly matched if every opening bracket in the string can be paired up with a later closing bracket, 
+and vice versa. For instance, “(())()” is correctly matched, whereas “)(“ and “((” aren’t. For instance, “((” could become correctly matched by adding two closing 
+brackets at the end, so you’d return 2.
+Given a string that consists of brackets, write a function bracketMatch that takes a bracket string as an input and returns 
+the minimum number of brackets you’d need to add to the input in order to make it correctly matched.
+'''
 def bracket_match(text):
   
   diff, mis = 0, 0
