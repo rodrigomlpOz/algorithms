ecd48cf13b9c43d2656cd6f5bf2b5227983a1678
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-01-17 13:09:09 -0800

    Template

--- tree/inorder_iterative.py
+++ tree/inorder_iterative.py
@@ -1,12 +1,17 @@
-def inorder_traversal(tree):
-    s, result = [], []
+def inorder_traversal(root):
+    stack = []
+    result = []
 
-    while s or tree:
-        if tree:
-            s.append(tree)
-            tree = tree.left
+    if not root:
+        return result
+
+    curr_node = root
+    while stack or curr_node:
+        if curr_node:
+            stack.append(curr_node)
+            curr_node = curr_node.left
         else:
-            tree = s.pop()
-            result.append(tree.data)
-            tree = tree.right
+            curr_node = stack.pop()
+            result.append(curr_node.val)
+            curr_node = curr_node.right
     return result
