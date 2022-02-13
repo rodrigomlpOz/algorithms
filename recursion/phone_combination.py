850a45730bbf1da37fafd3304f2622722078d76c
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-02-12 19:42:21 -0800

    Update phone_combination.py

--- recursion/phone_combination.py
+++ recursion/phone_combination.py
@@ -1,3 +1,14 @@
+'''
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+'''
+
 class Solution:
     def letterCombinations(self, digits):
         """
