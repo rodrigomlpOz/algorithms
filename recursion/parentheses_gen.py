420613afbbe9ee69a99f6e845bae877a53bc19b8
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-02-13 00:24:31 -0800

    Update parentheses_gen.py

--- recursion/parentheses_gen.py
+++ recursion/parentheses_gen.py
@@ -1,5 +1,5 @@
 '''
-iven n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
  
 
