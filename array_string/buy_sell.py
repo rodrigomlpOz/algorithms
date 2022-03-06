a2006e22785c49c3736205b652b409afb749756c
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-03-05 20:36:04 -0800

    Update buy_sell.py

--- array_string/buy_sell.py
+++ array_string/buy_sell.py
@@ -3,10 +3,9 @@ https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 '''
 
 def buy_and_sell_stock_once(prices):
-
-    min_price_so_far, max_profit = float('inf'), 0.0
-    for price in prices:
-        max_profit_sell_today = price - min_price_so_far
-        max_profit = max(max_profit, max_profit_sell_today)
-        min_price_so_far = min(min_price_so_far, price)
-    return max_profit
+        min_so_far = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            min_so_far = min(min_so_far, prices[i])
+            max_profit = max(max_profit, prices[i] - min_so_far)
+        return max_profit
