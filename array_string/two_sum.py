4fa4f32b4cef9471612d616bf5cc3e18aaab2ee0
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-03-05 20:34:38 -0800

    Create two_sum.py

---
+++ array_string/two_sum.py
@@ -0,0 +1,11 @@
+'''
+https://leetcode.com/problems/two-sum/solution/
+'''
+
+def twoSum(nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
