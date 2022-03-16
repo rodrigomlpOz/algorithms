165327dde6b294a1c2908e45830cf4d20512ae78
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-03-15 21:08:54 -0700

    Update decode_ways_recursion.py

--- dynamicprogramming/decode_ways_recursion.py
+++ dynamicprogramming/decode_ways_recursion.py
@@ -2,24 +2,19 @@
 https://leetcode.com/problems/decode-ways/
 '''
 
-def decode_ways(word):
-    def recurse(word):
-        if not word:
+def numDecodings(s):
+        def isPossibleDouble(num):
+            return len(num) == 2 and (num[0] == '1' or (num[0] == '2' and num[1] <= '6'))
+    
+        if s == '':
             return 1
-        if len(word) == 1:
-            return 1 if word[0] != '0' else 0
-        else:
-            for _ in range(len(word)-1):
-                one_digit = recurse(word[1:]) if word[0] != '0' else 0
-                two_digit = recurse(word[2:]) if 10 <= int(word[:2]) <= 26 else 0
-                return one_digit + two_digit
-    return recurse(word)
 
-'''
-Input: "12"
-Output: 2
-Explanation: It could be decoded as "AB" (1 2) or "L" (12)
-.
-'''
-ans = decode_ways("01")
-print(ans)
+        if s[0] == '0':
+            return 0
+
+        possibleDouble = True if isPossibleDouble(s[:2]) else False
+
+        if possibleDouble:
+            return numDecodings(s[1:]) + numDecodings(s[2:])
+        else:
+            return numDecodings(s[1:])
