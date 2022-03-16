97f8cc84d9c0413c71d046eb4d61036f5936a33b
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-03-15 21:00:52 -0700

    Update decode_ways_dp.py

--- dynamicprogramming/decode_ways_dp.py
+++ dynamicprogramming/decode_ways_dp.py
@@ -11,20 +11,21 @@ A -> 1 .... Z -> 26
 
 '''
 
-def decode_ways(S):
-    if not S:
-        return 0
-    dp = [0 for _ in range(len(S)+1)]
-    dp[0] = 1
-    dp[1] = 0 if S[0] == "0" else 1
-    for i in range(2, len(S)+1):
-        dp[i] = dp[i-1]
-        if 10 <= int(S[i-2:i]) <= 26:
-            dp[i] += dp[i-2]
-    return dp[len(S)]
-        
+def numDecodings(s): 
+	if not s:
+		return 0
 
-S = "1261"
+	dp = [0 for x in range(len(s) + 1)] 
+	
+	# base case initialization
+	dp[0] = 1 
+	dp[1] = 0 if s[0] == "0" else 1   #(1)
 
-r = decode_ways(S)
-print(r)
+	for i in range(2, len(s) + 1): 
+		# One step jump
+		if 0 < int(s[i-1:i]) <= 9:    #(2)
+			dp[i] += dp[i - 1]
+		# Two step jump
+		if 10 <= int(s[i-2:i]) <= 26: #(3)
+			dp[i] += dp[i - 2]
+	return dp[len(s)]
