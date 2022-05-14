c1ba123b2bb4b3fa8b6821ef49355176a8881c7f
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2022-05-14 14:03:59 -0700

    LRU cache

--- hashmap/lru_cache.py
+++ hashmap/lru_cache.py
@@ -1,47 +1,97 @@
-class Node:
-  def __init__(self, k, v):
-      self.key = k
-      self.val = v
-      self.prev = None
-      self.next = None
+class DLinkedNode(): 
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
+            
+class LRUCache():
+    def _add_node(self, node):
+        """
+        Always add the new node right after head.
+        """
+        node.prev = self.head
+        node.next = self.head.next
 
-class LRUCache:
-  def __init__(self, capacity):
-      self.capacity = capacity
-      self.dic = dict()
-      self.head = Node(0, 0)
-      self.tail = Node(0, 0)
-      self.head.next = self.tail
-      self.tail.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
 
-  def get(self, key):
-      if key in self.dic:
-          n = self.dic[key]
-          self._remove(n)
-          self._add(n)
-          return n.val
-      return -1
+    def _remove_node(self, node):
+        """
+        Remove an existing node from the linked list.
+        """
+        prev = node.prev
+        new = node.next
 
-  def set(self, key, value):
-      if key in self.dic:
-          self._remove(self.dic[key])
-      n = Node(key, value)
-      self._add(n)
-      self.dic[key] = n
-      if len(self.dic) > self.capacity:
-          n = self.head.next
-          self._remove(n)
-          del self.dic[n.key]
+        prev.next = new
+        new.prev = prev
 
-  def _remove(self, node):
-      p = node.prev
-      n = node.next
-      p.next = n
-      n.prev = p
+    def _move_to_head(self, node):
+        """
+        Move certain node in between to the head.
+        """
+        self._remove_node(node)
+        self._add_node(node)
 
-  def _add(self, node):
-      p = self.tail.prev
-      p.next = node
-      self.tail.prev = node
-      node.prev = p
-      node.next = self.tail
+    def _pop_tail(self):
+        """
+        Pop the current tail.
+        """
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.cache = {}
+        self.size = 0
+        self.capacity = capacity
+        self.head, self.tail = DLinkedNode(), DLinkedNode()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        node = self.cache.get(key, None)
+        if not node:
+            return -1
+
+        # move the accessed node to the head;
+        self._move_to_head(node)
+
+        return node.value
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        node = self.cache.get(key)
+
+        if not node: 
+            newNode = DLinkedNode()
+            newNode.key = key
+            newNode.value = value
+
+            self.cache[key] = newNode
+            self._add_node(newNode)
+
+            self.size += 1
+
+            if self.size > self.capacity:
+                # pop the tail
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+        else:
+            # update the value.
+            node.value = value
+            self._move_to_head(node)
