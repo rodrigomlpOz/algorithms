0142ec50401f10b384b81745d9f662e13a23f30a
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-06-11 16:00:43 -0700

    Update buy_sell.py

--- array_string/buy_sell.py
+++ array_string/buy_sell.py
@@ -23,3 +23,7 @@ def buy_and_sell_stock_once(prices):
             min_so_far = min(min_so_far, prices[i])
             max_profit = max(max_profit, prices[i] - min_so_far)
         return max_profit
+
+print(buy_and_sell_stock_once([7, 1, 5, 3, 6, 4]))  # Expected output: 5
+print(buy_and_sell_stock_once([7, 1, 5, 3, 6, 4]))  # Expected output: 5
+print(buy_and_sell_stock_once([7, 6, 4, 3, 1]))  # Expected output: 0
