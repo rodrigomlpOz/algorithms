5f703b98be4ae1a14b7ccbc4ba5644c2fe43090d
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-06-26 20:18:39 -0700

    Update and rename array_string/buy_sell_twice.py to array_string/BuySell2/buy_sell_twice.py
    buy sell twice

--- array_string/BuySell2/buy_sell_twice.py
+++ array_string/BuySell2/buy_sell_twice.py
@@ -1,17 +1,3 @@
-'''
-Problem Statement
-You are given an array prices where prices[i] is the price of a given stock on the i-th day.
-
-Find the maximum profit you can achieve. You may complete at most two transactions.
-
-Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
-
-Input: prices = [3,3,5,0,0,3,1,4]
-Output: 6
-Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
-             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
-
-'''
 def maxProfit(prices):
     if not prices:
         return 0
