a2425e632f510f5c17cef83279f19e79d4910288
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-06-27 22:28:58 -0700

    Update count_say.py
    func calls

--- array_string/CountSay/count_say.py
+++ array_string/CountSay/count_say.py
@@ -1,24 +1,29 @@
-'''
-EPI 6.8
-'''
-
-def count_say(n):
-    def next_say(s):
-        result, i = [], 0
-        while i < len(s):
-            count = 1
-            while i + 1 < len(s) and s[i] == s[i + 1]:
-                i += 1
+def countAndSay(n: int) -> str:
+    if n == 1:
+        return "1"
+    
+    previous = "1"
+    for _ in range(n - 1):
+        current = ""
+        count = 1
+        for j in range(1, len(previous)):
+            if previous[j] == previous[j - 1]:
                 count += 1
-            result.append(str(count) + s[i])
-            i += 1
-        return ''.join(result)
+            else:
+                current += str(count) + previous[j - 1]
+                count = 1
+        current += str(count) + previous[-1]
+        previous = current
+    
+    return previous
 
-
-
-    ans = "1"
-    for _ in range(1, n):
-        ans = next_say(ans)
-    return ans
-
-count_say(4)
+print(countAndSay(1))  # Output: "1"
+print(countAndSay(2))  # Output: "11"
+print(countAndSay(3))  # Output: "21"
+print(countAndSay(4))  # Output: "1211"
+print(countAndSay(5))  # Output: "111221"
+print(countAndSay(6))  # Output: "312211"
+print(countAndSay(7))  # Output: "13112221"
+print(countAndSay(8))  # Output: "1113213211"
+print(countAndSay(9))  # Output: "31131211131221"
+print(countAndSay(10)) # Output: "13211311123113112211"
