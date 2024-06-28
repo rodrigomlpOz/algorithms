2dc9ba1d513af7f8582ad0deee292a823891ee0a
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-06-27 22:36:47 -0700

    Update dutch.py
    dutch flag

--- array_string/Dutch/dutch.py
+++ array_string/Dutch/dutch.py
@@ -1,12 +1,8 @@
-'''
-EPI 5.1
-'''
-
 def dutch(A, index):
     val = A[index]
     less = 0
     equal = 0
-    more = len(A) 
+    more = len(A)
 
     while equal < more:
         if A[equal] < val:
@@ -18,9 +14,16 @@ def dutch(A, index):
             A[equal], A[more] = A[more], A[equal]
         else:
             equal += 1
-    print(A)
+    return A
 
-A = [0, 1, 2, 0, 2, 1, 1]
-index = 1
-ans = dutch_flag_loop(A, index)
-print(ans)
+A1 = [0, 1, 2, 0, 2, 1, 1]
+index1 = 1
+print(dutch(A1, index1))  # Output: [0, 0, 1, 1, 1, 2, 2]
+
+A2 = [3, 3, 2, 1, 3, 2, 1]
+index2 = 2
+print(dutch(A2, index2))  # Output: [1, 1, 2, 3, 3, 3, 2]
+
+A3 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+index3 = 6
+print(dutch(A3, index3))  # Output: [1, 1, 2, 3, 3, 4, 5, 9, 5, 6, 5]
