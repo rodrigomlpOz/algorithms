b77bfd68a1ef5e2bebc3c6ab86d38aa05b0a7aed
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-06-30 22:47:34 -0700

    Update find_duplicate.py

--- array_string/FindDuplicaate/find_duplicate.py
+++ array_string/FindDuplicaate/find_duplicate.py
@@ -1,15 +1,22 @@
 def findDuplicate(nums):
-        tortoise = hare = nums[0]
-        while True:
-            tortoise = nums[tortoise]
-            hare = nums[nums[hare]]
-            if tortoise == hare:
-                break
-        
-        # Find the "entrance" to the cycle.
-        tortoise = nums[0]
-        while tortoise != hare:
-            tortoise = nums[tortoise]
-            hare = nums[hare]
-        
-        return hare
+    tortoise = hare = nums[0]
+    while True:
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        if tortoise == hare:
+            break
+    
+    # Find the "entrance" to the cycle.
+    tortoise = nums[0]
+    while tortoise != hare:
+        tortoise = nums[tortoise]
+        hare = nums[hare]
+    
+    return hare
+
+# Test cases
+print(findDuplicate([1, 3, 4, 2, 2]))  # Output: 2
+print(findDuplicate([3, 1, 3, 4, 2]))  # Output: 3
+print(findDuplicate([1, 1]))           # Output: 1
+print(findDuplicate([1, 4, 6, 5, 3, 2, 4]))  # Output: 4
+print(findDuplicate([5, 4, 3, 2, 1, 5]))     # Output: 5
