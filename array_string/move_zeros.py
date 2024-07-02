34a0c9921d2f037979bfb0fdc6b3e398c0734276
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-07-02 16:53:12 -0700

    Update move_zeros.py

--- array_string/move_zeros.py
+++ array_string/move_zeros.py
@@ -1,22 +1,9 @@
-'''
-Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-Note that you must do this in-place without making a copy of the array.
- '''
- def moveZeroes(nums):
-        write = 0
-        for read in range(len(nums)):
-            if nums[read] != 0:
-                nums[read], nums[write] = nums[write], nums[read]
-                write += 1
-
-         
- def moveZeroes(nums):
-     lastNonZeroFoundAt = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-               nums[lastNonZeroFoundAt] = nums[i]
-               lastNonZeroFoundAt += 1
-        
-        for j in range(lastNonZeroFoundAt, len(nums)):
-              nums[j] = 0;
+def moveZeroes(nums):
+    lastNonZeroFoundAt = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[lastNonZeroFoundAt] = nums[i]
+            lastNonZeroFoundAt += 1
+    
+    for j in range(lastNonZeroFoundAt, len(nums)):
+        nums[j] = 0
