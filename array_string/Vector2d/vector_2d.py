1ba3b62f77d4d41659c5b0bdd240ac421c052c84
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-07-28 21:42:45 -0700

    Update vector_2d.py

--- array_string/Vector2d/vector_2d.py
+++ array_string/Vector2d/vector_2d.py
@@ -1,40 +1,30 @@
-'''
-https://aaronice.gitbook.io/lintcode/data_structure/flatten-2d-vector
-'''
 class Vector2D:
-
     def __init__(self, v):
         self.vector = v
-        self.inner = 0
         self.outer = 0
-    
-    # If the current outer and inner point to an integer, this method does nothing.
-    # Otherwise, inner and outer are advanced until they point to an integer.
-    # If there are no more integers, then outer will be equal to vector.length
-    # when this method terminates.
-    def advance_to_next(self):
-        # While outer is still within the vector, but inner is over the 
-        # end of the inner list pointed to by outer, we want to move
-        # forward to the start of the next inner vector.
-        while self.outer < len(self.vector) and self.inner == len(self.vector[self.outer]):
+        self.inner = 0
+        self.advance_to_next_nonempty()
+
+    def advance_to_next_nonempty(self):
+        # This method advances the outer and inner indices to point to the next
+        # non-empty sub-list element. If the current sub-list is exhausted or empty,
+        # it moves to the next sub-list until it finds a valid position or reaches the end.
+        while self.outer < len(self.vector) and self.inner >= len(self.vector[self.outer]):
             self.outer += 1
             self.inner = 0
-        
+
     def next(self):
-        # Ensure the position pointers are moved such they point to an integer,
-        # or put outer = vector.length.
-        self.advance_to_next()
-        # Return current element and move inner so that is after the current
-        # element.
+        # Returns the next element in the 2D vector and advances the indices.
+        if not self.hasNext():
+            raise Exception("No more elements")
         result = self.vector[self.outer][self.inner]
         self.inner += 1
+        self.advance_to_next_nonempty()  # Prepare indices for the next call
         return result
-        
 
     def hasNext(self):
-        # Ensure the position pointers are moved such they point to an integer,
-        # or put outer = vector.length.
-        self.advance_to_next()
-        # If outer = vector.length then there are no integers left, otherwise
-        # we've stopped at an integer and so there's an integer left.
+        # Checks if there are more elements available in the vector.
+        # This method ensures that the indices point to a valid element or the end.
+        self.advance_to_next_nonempty()
         return self.outer < len(self.vector)
+
