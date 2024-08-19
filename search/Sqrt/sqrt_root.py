72a50966118d6f2005094fc1bcc2256632609db3
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-08-19 16:58:50 -0700

    Update sqrt_root.py

--- search/Sqrt/sqrt_root.py
+++ search/Sqrt/sqrt_root.py
@@ -1,17 +1,16 @@
 def val_equal_index(num):
     l = 0
-    h = num-1
-    mid = (h+l)//2
+    h = num
     while l <= h:
-        mid = (h+l)//2
-        if (mid)*(mid) <= num < (mid+1)*(mid+1):
+        mid = (l + h) // 2
+        if mid * mid <= num < (mid + 1) * (mid + 1):
             return mid
-        elif (mid * mid) > num:
-            h = mid + 1
+        elif mid * mid > num:
+            h = mid - 1
         else:
-            l = mid - 1
+            l = mid + 1
     return l
 
 num = 300
 ans = val_equal_index(num)
-print(ans)
+print(ans)  # Expected output: 17
