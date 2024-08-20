2b198d1e88c250a0b44bac0324c8e8852b3b39a1
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-08-19 17:00:57 -0700

    Update sqrt_root_float.py

--- search/SqrtFloat/sqrt_root_float.py
+++ search/SqrtFloat/sqrt_root_float.py
@@ -1,15 +1,17 @@
 def val_equal_index(num):
     l = 0
-    h = num-1
-    mid = (h+l)/2
-    while abs((mid*mid) - num) > 0.001:
-        mid = (h+l)/2
-        if (mid * mid) > num:
-            h = mid + 1
+    h = num
+    mid = (h + l) / 2
+    
+    while abs((mid * mid) - num) > 0.001:  # Adjust precision as needed
+        if mid * mid > num:
+            h = mid  # Move the high pointer down
         else:
-            l = mid - 1
+            l = mid  # Move the low pointer up
+        mid = (h + l) / 2  # Recalculate mid
+
     return mid
 
 num = 6
 ans = val_equal_index(num)
-print(ans)
+print(ans)  # Expected output: Approximately 2.449
