a320d1f6d7a66bced53d94c2d78dfb7f248a7daa
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-08 14:21:32 -0700

    Update knapsack_dp.py

--- dynamicprogramming/knapsack_dp.py
+++ dynamicprogramming/knapsack_dp.py
@@ -11,8 +11,7 @@ def knapSack(W, wt, val, n):
             if i == 0 or w == 0: 
                 K[i][w] = 0
             elif wt[i-1] <= w: 
-                K[i][w] = max(val[i-1]  
-+ K[i-1][w-wt[i-1]],  K[i-1][w]) 
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]) 
             else: 
                 K[i][w] = K[i-1][w] 
   
