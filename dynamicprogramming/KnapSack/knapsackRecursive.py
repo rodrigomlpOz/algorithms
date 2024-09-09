86deb2d81b589adbd7b0f99ce3985ef84e5857dd
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-09 14:31:55 -0700

    Update knapsackRecursive.py

--- dynamicprogramming/KnapSack/knapsackRecursive.py
+++ dynamicprogramming/KnapSack/knapsackRecursive.py
@@ -1,36 +1,35 @@
-# A naive recursive implementation 
-# of 0-1 Knapsack Problem 
-  
-# Returns the maximum value that  
-# can be put in a knapsack of  
-# capacity W 
-def knapSack(W, wt, val, n): 
-  
-    # Base Case 
-    if n == 0 or W == 0 : 
+def knapSack(W, wt, val, n):
+    """
+    A recursive function to solve the 0-1 Knapsack Problem in a forward manner.
+
+    Args:
+    W : int : The total capacity of the knapsack.
+    wt : list : List of item weights.
+    val : list : List of item values corresponding to the weights.
+    n : int : The number of items to choose from.
+
+    Returns:
+    int : The maximum value that can be obtained with the given capacity.
+    """
+    # Base cases: if the list of items is empty or no capacity is left
+    if n == 0 or W == 0:
         return 0
-  
-    # If weight of the nth item is  
-    # more than Knapsack of capacity W,  
-    # then this item cannot be included  
-    # in the optimal solution 
-    if (wt[n-1] > W): 
-        return knapSack(W, wt, val, n-1) 
-  
-    # return the maximum of two cases: 
-    # (1) nth item included 
-    # (2) not included 
-    else: 
-        return max( 
-            val[n-1] + knapSack( 
-                W-wt[n-1], wt, val, n-1),  
-                knapSack(W, wt, val, n-1)) 
-  
-# end of function knapSack 
-  
-# To test above function 
-val = [60, 100, 120] 
-wt = [10, 20, 30] 
+
+    # If the weight of the current item is more than the remaining capacity, skip it
+    if wt[0] > W:
+        return knapSack(W, wt[1:], val[1:], n - 1)
+    
+    # Otherwise, we have two choices:
+    # 1. Take the current item and proceed to the next item with reduced capacity
+    # 2. Skip the current item and proceed to the next item with the same capacity
+    return max(
+        val[0] + knapSack(W - wt[0], wt[1:], val[1:], n - 1),  # Include current item
+        knapSack(W, wt[1:], val[1:], n - 1)                   # Skip current item
+    )
+
+# Example call
 W = 50
-n = len(val) 
-print knapSack(W, wt, val, n) 
+wt = [10, 20, 30]
+val = [60, 100, 120]
+n = len(wt)
+print(knapSack(W, wt, val, n))  # Output: 220
