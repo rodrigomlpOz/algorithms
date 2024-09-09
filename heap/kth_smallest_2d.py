b879692513b4b33f8becc29c8aa5ebe27cc898d5
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-08 23:15:48 -0700

    Update kth_smallest_2d.py

--- heap/kth_smallest_2d.py
+++ heap/kth_smallest_2d.py
@@ -1,30 +1,25 @@
 import heapq
+
 def kth_two(matrix, k):
     result, heap = None, []
+    n = len(matrix)
+
+    # Push the first element from the matrix into the heap along with its position
     heapq.heappush(heap, (matrix[0][0], 0, 0))
+    
     while k > 0:
+        # Pop the smallest element from the heap
         result, i, j = heapq.heappop(heap)
-        if i == 0 and j + 1 < len(matrix):
+
+        # Push the next element in the same row if it exists
+        if i == 0 and j + 1 < n:
             heapq.heappush(heap, (matrix[i][j + 1], i, j + 1))
-        if i + 1 < len(matrix):
+
+        # Push the next element in the same column if it exists
+        if i + 1 < n:
             heapq.heappush(heap, (matrix[i + 1][j], i + 1, j))
+        
+        # Decrease k as we are processing one element at a time
         k -= 1
+
     return result
-
-
-
-matrix = [[ 1,  5,  9],
-   [10, 11, 13],
-   [12, 13, 15]
-]
-k = 8
-ans = kth_two(matrix, k)
-print(ans)
-'''
-matrix = [
-   [ 1,  5,  9],
-   [10, 11, 13],
-   [12, 13, 15]
-],
-k = 8,
-'''
