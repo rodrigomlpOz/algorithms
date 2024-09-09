12adb29119a851ce1ea41fe941c6adef3d55bc26
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-08 22:23:00 -0700

    Update word_break_recursion.py

--- dynamicprogramming/WordBreak/word_break_recursion.py
+++ dynamicprogramming/WordBreak/word_break_recursion.py
@@ -9,17 +9,3 @@ def word_break(s, wordDict):
             return False
     return recurse(s, wordDict)
 
-def word_break_dp(s, wordDict):
-    dp = [False] * (len(s)+1)
-    dp[0] = True
-    for i in range(len(s)):
-        if dp[i]:
-            for j in range(i+1, len(s)+1):
-                if s[i:j] in wordDict:
-                    dp[j] = True
-        
-    return dp[-1]
-
-s = "applepenapple"
-wordDict = ["apple", "pen"]
-word_break_dp(s, wordDict)
