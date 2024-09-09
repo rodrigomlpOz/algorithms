930a3fbf5711994e1bdbf889bddb7b489bd62cc8
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-09 13:29:01 -0700

    Update perfect_square_dp.py

--- dynamicprogramming/perfect_square_dp.py
+++ dynamicprogramming/perfect_square_dp.py
@@ -1,23 +1,22 @@
 import math
-def numSquares(n):
-    square_nums = [i**2 for i in range(0, int(math.sqrt(n))+1)]
-        
-    dp = [float('inf')] * (n+1)
-    # bottom case
-    dp[0] = 0
+
+def numSquares(n: int) -> int:
+    # DP array to store the least number of perfect squares for each value up to n
+    dp = [float('inf')] * (n + 1)
+    dp[0] = 0  # base case: 0 can be represented by 0 squares
+
+    # Precompute all square numbers up to n
+    square_nums = [i**2 for i in range(1, int(math.sqrt(n)) + 1)]
     
-    for i in range(1, n+1):
+    # Fill the DP array
+    for i in range(1, n + 1):
         for square in square_nums:
             if i < square:
                 break
-            dp[i] = min(dp[i], dp[i-square] + 1)
+            dp[i] = min(dp[i], dp[i - square] + 1)
     
-    return dp[-1]
+    return dp[n]
 
-n = 12
-ans = numSquares(n)
-print(ans)
-'''
-Input: n = 12
-Output: 3 
-'''
+# Example calls
+print(numSquares(12))  # Output: 3
+print(numSquares(13))  # Output: 2
