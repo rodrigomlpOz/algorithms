191e4942ae56e2a113c1b0698f9bfa3dacf18e8f
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-30 12:05:07 -0700

    Update queue_max.py

--- stack_queue/QueueMax/queue_max.py
+++ stack_queue/QueueMax/queue_max.py
@@ -1,24 +1,32 @@
-import collections
-class QueueWithMax:
+from collections import deque
+
+class QueueMax:
     def __init__(self):
-        self._entries = collections.deque()
-        self._candidates_for_max = collections.deque()
+        self.queue = deque()       # Regular queue for enqueue/dequeue
+        self.max_queue = deque()   # Deque to store the maximum values
 
     def enqueue(self, x):
-        self._entries.append(x)
-        # Eliminate dominated elements in _candidates_for_max.
-        while self._candidates_for_max and self._candidates_for_max[-1] < x:
-            self._candidates_for_max.pop()
-        self._candidates_for_max.append(x)
+        # Add the element to the regular queue
+        self.queue.append(x)
+        
+        # Remove all elements from the back of the max_queue that are smaller than the new element
+        while self.max_queue and self.max_queue[-1] < x:
+            self.max_queue.pop()
+        
+        # Add the new element to the back of max_queue
+        self.max_queue.append(x)
 
     def dequeue(self):
-        result = self._entries.popleft()
-        if result == self._candidates_for_max[0]:
-            self._candidates_for_max.popleft()
-        return result
-
+        # Remove the front element from the regular queue
+        if self.queue:
+            front = self.queue.popleft()
+            
+            # If the element being removed is the same as the front of max_queue, remove it from max_queue as well
+            if front == self.max_queue[0]:
+                self.max_queue.popleft()
+    
     def max(self):
-        return self._candidates_for_max[0]
-
-    def head(self):
-        return self._entries[0]
+        # Return the front element of max_queue, which is the maximum value in the queue
+        if self.max_queue:
+            return self.max_queue[0]
+        return None  # If queue is empty, return None
