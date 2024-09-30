21767ce305a196b0be26e7d0ae593c74fae80785
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-09-30 16:55:00 -0700

    Delete sorting/NonConstructible directory

--- sorting/NonConstructible/non_constructible.py
+++
@@ -1,7 +0,0 @@
-def smallest_nonconstructible_value(A):
-    max_constructible_value = 0
-    for a in sorted(A):
-        if a > max_constructible_value + 1:
-            break
-        max_constructible_value += a
-    return max_constructible_value + 1
