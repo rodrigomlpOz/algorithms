d9e3727b1cd2c6b489b1b12f70374b1be2935b35
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-10-07 17:42:36 -0700

    Update lca_bst.py

--- binarysearchtree/LCA/lca_bst.py
+++ binarysearchtree/LCA/lca_bst.py
@@ -1,11 +1,11 @@
 # EPI 14.4 
 def find_lca(tree, s, b):
+    while tree:
+        if s.data <= tree.data <= b.data:
+            return tree  # Found the LCA
+        elif tree.data < s.data:  # Move right if tree's data is smaller than s
+            tree = tree.right
+        else:  # Move left if tree's data is larger than b
+            tree = tree.left
+    return None  # Return None if no LCA is found
 
-    while tree.data < s.data or tree.data > b.data:
-        # Keep searching since tree is outside of [s, b].
-        while tree.data < s.data:
-            tree = tree.right  # LCA must be in tree's right child.
-        while tree.data > b.data:
-            tree = tree.left  # LCA must be in tree's left child.
-    # Now, s.data <= tree.data && tree.data <= b.data.
-    return tree
