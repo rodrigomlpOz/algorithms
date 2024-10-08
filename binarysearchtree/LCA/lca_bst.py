57cebec5ede911769a26d17b908dfc9bbb972f0e
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-10-08 01:02:37 -0700

    Update lca_bst.py

--- binarysearchtree/LCA/lca_bst.py
+++ binarysearchtree/LCA/lca_bst.py
@@ -1,11 +1,12 @@
-# EPI 14.4 
-def find_lca(tree, s, b):
-    while tree:
-        if s.data <= tree.data <= b.data:
-            return tree  # Found the LCA
-        elif tree.data < s.data:  # Move right if tree's data is smaller than s
-            tree = tree.right
-        else:  # Move left if tree's data is larger than b
-            tree = tree.left
-    return None  # Return None if no LCA is found
-
+def lowestCommonAncestor(root, p, q) -> TreeNode:
+    # Iterate through the BST
+    while root:
+        if p.val < root.val and q.val < root.val:
+            # If both nodes are smaller than the root, move to the left subtree
+            root = root.left
+        elif p.val > root.val and q.val > root.val:
+            # If both nodes are larger than the root, move to the right subtree
+            root = root.right
+        else:
+            # We've found the split point, so this is the LCA
+            return root
