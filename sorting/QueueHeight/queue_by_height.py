740661a46c1cf6c717f5d587a83c9c7e5a862539
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-10-18 01:34:00 -0700

    Delete sorting/QueueHeight directory

--- sorting/QueueHeight/queue_by_height.py
+++
@@ -1,7 +0,0 @@
-def reconstructQueue(people):
-        people.sort(key = lambda x: (-x[0], x[1]))
-        output = []
-        for p in people:
-            output.insert(p[1], p)
-        return output
-        
