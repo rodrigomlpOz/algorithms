e38296e4c4656165acbf3cbcf77f95055d6644fd
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2024-10-21 01:34:53 -0700

    Update minSize.py

--- sliding_window/MinSizeSubarraySum/minSize.py
+++ sliding_window/MinSizeSubarraySum/minSize.py
@@ -13,3 +13,7 @@ def minSubArrayLen(target: int, nums: list[int]) -> int:
             left += 1
 
     return min_length if min_length != float("inf") else 0
+
+# Why while?
+# Repeated Condition Checking: The while loop allows for continuous checking of the condition (current_sum >= target) and keeps executing as long as the condition remains true. This is necessary here because once the current_sum reaches or exceeds the target, it might still be possible to shrink the window further to achieve an even smaller valid subarray.
+# Dynamic Window Adjustment: If an if statement were used, it would only perform the check and adjustment once, then move on. However, there may be cases where multiple elements need to be removed from the left side for the current_sum to fall below the target again.
